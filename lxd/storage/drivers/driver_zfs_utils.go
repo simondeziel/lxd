8a3d5070575827e99a2b64e5320683b0ee517641
Simon Deziel <simon.deziel@canonical.com>
2025-09-09 09:41:28 -0400

    lxd/storage/drivers/zfs_utils: simplify command building in `sendDataset()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/driver_zfs_utils.go
+++ lxd/storage/drivers/driver_zfs_utils.go
@@ -356,8 +356,7 @@ func (d *zfs) sendDataset(dataset string, parent string, volSrcArgs *migration.V
 		args = append(args, "-i", parent)
 	}
 
-	args = append(args, dataset)
-	cmd := exec.Command("zfs", args...)
+	cmd := exec.Command("zfs", append(args, dataset)...)
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
