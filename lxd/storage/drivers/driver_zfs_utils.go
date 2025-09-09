02c072be758933aebbcad15fa8ae4d5ac3a92331
Simon Deziel <simon.deziel@canonical.com>
2025-09-09 09:38:12 -0400

    lxd/storage/drivers/zfs_utils: simplify return of `createDataset()`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- lxd/storage/drivers/driver_zfs_utils.go
+++ lxd/storage/drivers/driver_zfs_utils.go
@@ -76,11 +76,7 @@ func (d *zfs) createDataset(dataset string, options ...string) error {
 	args = append(args, dataset)
 
 	_, err := shared.RunCommandContext(context.TODO(), "zfs", args...)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func (d *zfs) createVolume(dataset string, size int64, options ...string) error {
