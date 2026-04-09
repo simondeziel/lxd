8c5ada9a8e2903c1a68d25d3d85aa4c0fa7e3329
Simon Deziel <simon.deziel@canonical.com>
2026-04-09 19:41:54 -0400

    github: switch to pinned `aquasecurity/trivy-action`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- .github/actions/install-trivy/trivy-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-exec /usr/bin/docker run --rm \
-  --user "$(id -u):$(id -g)" \
-  -v /home/runner/vuln-cache:/cache \
-  -v "$GITHUB_WORKSPACE":"$GITHUB_WORKSPACE" \
-  -w "$PWD" \
-  "${TRIVY_IMAGE}" \
-  --cache-dir /cache "$@"
